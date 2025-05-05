1fd6099faaab47281d7e3bc80812c9a648e7635b
Sigma Square <sigmasquare@192.168.1.2>
2025-05-05 16:44:35 +0500

    fix high volume issues

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -1,4 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['recaptcha_secret'] = '6Lfvg2EUAAAAAJU3gsQsoww5x0uaHXuRDTFPYJhv';
+// $config['recaptcha_secret'] = '6Lfvg2EUAAAAAJU3gsQsoww5x0uaHXuRDTFPYJhv';
+$config['recaptcha_secret'] = getenv('RECAPTCHA_SECRET_KEY');
